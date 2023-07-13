b8c1d4f15f382277a0b2931699e4dd3fab947201
Dre Tay <andretaylor681@gmail.com>
2023-07-13 03:17:53 -0400

    added array with questions and answers

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,26 @@ console.log(startButton)
 var timeEl = document.querySelector(".time");
 var quiz = document.querySelector("#quiz-container");
 var startTime = 60
+var question = document.querySelector('.question')
+var questions
+=[
+     {question:"Which operator is for strict equality",
+     answer:['==','!=','===','none of the above']},
+   
+     {question:"Which of the following keywords is used to define a variable in Javascript?",
+     answer:['var','element','x','none of the above']},
+
+     
+     {question:"When an operator value is NULL, the typeof returned is:",
+     answer:['boolean','undefined','object','none of the above']},
+
+    
+     {question:"How do we write a comment in javascript",
+     answer:['/* */','$','" "','none of the above']}, 
+]
+
+
+
 
 
 
@@ -25,11 +45,28 @@ startButton.addEventListener("click",function(event){
 
       if(event){
         startButton.setAttribute("class",'hide');
-        quiz.setAttribute('class','')
+        quiz.setAttribute('class','');
+    
+        }
+        
         
 
-    
+        
 }     
+
+function setNextQuestion(){
+
+};
+function showQuestion(){
+
+};
+
+function selectAnswer(){
+
+};
+
+
+
 });
 
 
