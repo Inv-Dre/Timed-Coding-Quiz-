f3b3752f4c07d64fbf753c6acec3c2a7dc46f310
Dre Tay <andretaylor681@gmail.com>
2023-07-13 14:10:22 -0400

    removed html file from assets, changed my ul back to divs so ththey could be separate buttons, made addeventlisteners that would make questions appear(still need to make the work), when the eventlisteners were working they were not working in one click added code to fix that, if else statements werent working correctly either

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,11 @@ var startButton = document.querySelector("#start-btn")
 console.log(startButton)
 var timeEl = document.querySelector(".time");
 var quiz = document.querySelector("#quiz-container");
-var startTime = 60
-var question = document.querySelector('.question')
+var startTime = 60;
+var question = document.querySelector('.question');
+var score = 0;
+var hs = document.querySelector(".hs");
+var question = document.querySelector(".question");
 var questions
 =[
      {question:"Which operator is for strict equality",
@@ -19,7 +22,12 @@ var questions
     
      {question:"How do we write a comment in javascript",
      answer:['/* */','$','" "','none of the above']}, 
-]
+];
+
+var ans0 = document.querySelector('#a');
+var ans1 = document.querySelector('#b');
+var ans2 = document.querySelector('#c');
+var ans3 = document.querySelector('#d');
 
 
 
@@ -27,11 +35,12 @@ var questions
 
 
 startButton.addEventListener("click",function(event){
+    
         var timerInterval= setInterval(function(){
           startTime--;
           timeEl.textContent = startTime;
       
-          if(startTime===0){
+          if(startTime<=0){
               clearInterval(timerInterval);
               endQuiz();
           }
@@ -41,33 +50,100 @@ startButton.addEventListener("click",function(event){
       
       function endQuiz(){
           timeEl.textContent = "Times Up!!"
-      };
-
+      }
+          
       if(event){
         startButton.setAttribute("class",'hide');
         quiz.setAttribute('class','');
-    
+        hs.setAttribute('class','');
+
+        question.textContent= questions[0].question;
+        ans0.textContent = questions[0].answer[0];
+        ans1.textContent = questions[0].answer[1];
+        ans2.textContent = questions[0].answer[2];
+        ans3.textContent = questions[0].answer[3];
+        firstQuestion();
+      }});
+function firstQuestion(){
+        question.addEventListener("click",function(event){
+        var answer= event.target;
+        console.log(ans2);
+        console.log(answer);
+            if(answer !== ans2){
+                
+                startTime -=10;
+                
+            }else {
+               score += 10;
+            
+               
+            };
+            nextQuestion();
+    }, {once:true}
+
+)};
+
+function nextQuestion(){
+    question.addEventListener("click",function(event){
+
+    var answer= event.target
+        question.textContent= questions[1].question;
+      
+        ans0.textContent = questions[1].answer[0];
+        ans1.textContent = questions[1].answer[1];
+        ans2.textContent = questions[1].answer[2];
+        ans3.textContent = questions[1].answer[3];
+        
+        if(answer.class !== ans0){
+            startTime -=10;
         }
+        else {
+           score +=10;
+        };
+   
+        nextQuestion1();
         
-        
-
-        
-}     
-
-function setNextQuestion(){
-
-};
-function showQuestion(){
-
+    }, {once:true});
 };
 
-function selectAnswer(){
-
+function nextQuestion1(){
+    question.addEventListener("click",function(event){
+        var answer= event.target
+        question.textContent= questions[2].question;
+      
+        ans0.textContent = questions[2].answer[0];
+        ans1.textContent = questions[2].answer[1];
+        ans2.textContent = questions[2].answer[2];
+        ans3.textContent = questions[2].answer[3];
+        
+        if(answer !== ans1){
+            startTime -= 10;
+           
+        }
+        else {
+           score += 10;
+           
+        };
+        nextQuestion2();
+        
+    }, {once:true});
 };
 
+function nextQuestion2(){
+    ul.addEventListener("click",function(event){
+        var answer= event.target
+        question.textContent= questions[3].question;
+      
+        ans0.textContent = questions[3].answer[0];
+        ans1.textContent = questions[3].answer[1];
+        ans2.textContent = questions[3].answer[2];
+        ans3.textContent = questions[3].answer[3];
+        
+        if(answer !== ans3){
+            startTime -= 10;
+        }
+        else {
+          score += 10;};
 
-
-});
-
-
-
+    }, {once:true});
+};
